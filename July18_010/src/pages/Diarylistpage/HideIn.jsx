02d85c9da706021cb5b1e0ea782d90bc4d8d1db1
som <133095820+Park-SoMin@users.noreply.github.com>
2023-07-18 15:14:46 +0900

    7월 18일 010

---
+++ July18_010/src/pages/Diarylistpage/HideIn.jsx
@@ -0,0 +1,26 @@
+// 교내활동 리스트
+
+import React from "react";
+import './Diarylist.css';
+
+const HideOut = () => {
+    
+    return (
+        <>
+        <article className="List_Article3">
+          <div className="List_Listbox">
+            <div className="List_Title">교내활동</div>
+            <div className="List_Content">내용</div>
+          </div>
+          <hr className="List_Hr" />
+          <div className="List_Listbox">
+            <div className="List_Title">교내활동</div>
+            <div className="List_Content">내용</div>
+          </div>
+          <hr className="List_Hr" />
+        </article>
+        </>
+    );
+};
+
+export default HideOut;
