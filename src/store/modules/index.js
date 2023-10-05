eab2e9e1bda616b18e6fecd3b11df5d022ea98e0
howhaveubeen <133098575+howhaveubeen@users.noreply.github.com>
2023-10-05 16:22:37 +0900

    Delete src directory

--- src/store/modules/index.js
+++
@@ -1,2 +0,0 @@
-export { default as date } from "./date";
-export { default as todolist } from "./todolist";
--- src/store/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import * as modules from "./modules";
-
-const rootReducer = combineReducers(modules);
-
-export default rootReducer;
