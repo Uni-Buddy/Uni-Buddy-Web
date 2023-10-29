166ef499721972aace793a7a33be01b15901ac49
ohsarang <delay49@naver.com>
2023-10-30 03:15:19 +0900

    1030

--- src/main/java/BE/UniBuddy_crud/service/DiarywriteService.java
+++ src/main/java/BE/UniBuddy_crud/service/DiarywriteService.java
@@ -1,13 +1,10 @@
 package BE.UniBuddy_crud.service;
 
 import BE.UniBuddy_crud.domain.Diarywrite;
-import BE.UniBuddy_crud.domain.Users;
 import BE.UniBuddy_crud.repository.DiarywriteRepository;
-import BE.UniBuddy_crud.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 
 @RequiredArgsConstructor
 @Service
